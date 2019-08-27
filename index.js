842dd998bb15b2aed1477a217af3a2090291b19d
Thomas Emery <tkemery7@gmail.com>
2019-08-26 22:32:37 -0400

    Added an index.js linked index.html

---
+++ index.js
@@ -0,0 +1,32 @@
+
+$(function(){
+    
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        
+        const userInput = $(this).find('input')
+        $('.shopping-list').append(`
+        <li>
+            <span class="shopping-item">${userInput.val()}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+        </li>`);
+    });
+});
+
+$('ul').on('click','.shopping-item-toggle', function(event){
+    const theDiv = $(this).closest('div');
+    theDiv.prev().toggleClass('shopping-item__checked');
+});
+
+$('ul').on('click','.shopping-item-delete', function(event){
+    const theDiv = $(this).closest('div');
+    const theLi = theDiv.parent();
+    theLi.remove();
+})
